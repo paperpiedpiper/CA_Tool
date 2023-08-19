946ec99aa807b65b60daf451921606709c443b64
paperpiedpiper <105975348+paperpiedpiper@users.noreply.github.com>
2023-08-20 00:48:39 +0300

    v2 commit

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,47 @@
 "use strict";
 
-// var myNotificationID = null;
-let tabId = null;
-
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.message === "trigger") {
+  if (message.message == "trigger") {
     createNotification();
     console.log(message.message);
   }
 });
 
 function createNotification(){
-    const options = {
-      type: "basic",
-      title: "Alert",
-      message: "Unassigned tickets in!",
-      iconUrl: "alert.png",
-      buttons: [{
-        title: "Snooze for 10m",
-        iconUrl: "snooze1.png",
-      },
-    {
-      title: "Snooze for 20m",
-      iconUrl: "snooze2.png"
-    }]
-    };
+  const options = {
+    type: "basic",
+    title: "Alert",
+    message: "Unassigned tickets in!",
+    iconUrl: "images/alert.png",
+    buttons: [{
+      title: "Snooze for 10 minutes",
+      iconUrl: "images/snooze1.png",
+    },
+  {
+    title: "Snooze for 20 minutes",
+    iconUrl: "images/snooze2.png"
+  }]
+  };
 
-    chrome.tabs.query({ url: 'https://goodman-sd.stefanini.com/CAisd/pdmweb.exe' }, function (tabs) {
-      tabId = tabs[0].id;
-    });
-    
-    chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
-      // if (notifId === myNotificationID) {
-        if (btnIdx === 0) {
-          chrome.tabs.sendMessage(tabId, { message: "600000"});
-        }
-        else if (btnIdx === 1) {
-          chrome.tabs.sendMessage(tabId, { message: "1200000"});
-        }
-      // }
-    });
+  let tabId_handle;
+  chrome.tabs.query({ url: 'https://goodman-sd.stefanini.com/CAisd/pdmweb.exe' }, function (tabs) {
+    tabId_handle = tabs[0].id;
+  });
+  
+  chrome.notifications.create("unassignedAlert", options);
 
-    chrome.notifications.create("unassignedAlert", options);
+  chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+    if (notifId == "unassignedAlert") {
+      if (btnIdx == 0) {
+        chrome.tabs.sendMessage(tabId_handle, { message: "button1Clicked"});
+      }
+      else if (btnIdx == 1) {
+        chrome.tabs.sendMessage(tabId_handle, { message: "button2Clicked"});
+      }
+    }
+  });
 
-    //clears the notification after 7 seconds
-    setTimeout(function(){chrome.notifications.clear("unassignedAlert");}, 7000);
+  setTimeout(function(){
+    chrome.notifications.clear("unassignedAlert");
+  }, 7000);
 }
