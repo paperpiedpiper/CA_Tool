0a2047d7dabd5067214c58ccfadde37313a411cc
paperpiedpiper <ciprian.patriciu.paraschiv@gmail.com>
2023-08-19 23:38:02 +0300

    v2 commit

--- popup.js
+++ popup.js
@@ -4,6 +4,15 @@ document.querySelector("button#unsnooze").addEventListener("click", function () 
 
     chrome.tabs.query({ url: 'https://goodman-sd.stefanini.com/CAisd/pdmweb.exe' }, function (tabs) {
     const tabId = tabs[0].id;
-    chrome.tabs.sendMessage(tabId, { message: "popupUnSnooze" });
+    chrome.tabs.sendMessage(tabId, { message: "popupUnsnooze" });
+    });
+    window.close();
 });
+document.querySelector("button#half_hour_break").addEventListener("click", function () {
+
+    chrome.tabs.query({ url: 'https://goodman-sd.stefanini.com/CAisd/pdmweb.exe' }, function (tabs) {
+    const tabId = tabs[0].id;
+    chrome.tabs.sendMessage(tabId, { message: "30mBreak" });
+    });
+    window.close();
 });
